5f7df93fb7372bb86662e73bf86029868262caab
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-01-22 23:55:51 +0900

    jh

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,9 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
+      I18n.enforce_available_locales = false
+      config.i18n.default_locale = :ja 
+
     end
   end
 end
