acf98875d72cdca43a6ffa028fbc2d0081014542
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-02-10 15:57:04 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,48 @@
-$(function(){
+$(function(){ 
      function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="message-info">
-            <div class="name">
-              ${message.user_name}
+      if ( message.image ) {
+        var html =
+         `<div class="post-message" data-message-id=${message.id}>
+            <div class="message-info">
+              <div class="name">
+                ${message.user_name}
+              </div>
+              <div class="date">
+                ${message.created_at}
+              </div>
             </div>
-            <date">
-              ${message.created_at}
+            <div class="post">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
             </div>
-          </div>
-          <div class="post">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="message-info">
-            <div class="name">
-              ${message.user_name}
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="post-message" data-message-id=${message.id}>
+            <div class="message-info">
+              <div class="name">
+                ${message.user_name}
+              </div>
+              <div class="date">
+                ${message.created_at}
+              </div>
             </div>
-            <div class="date">
-              ${message.created_at}
+            <div class="post">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
             </div>
-          </div>
-          <div class="post">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
-  }
-
+          </div>`
+        return html;
+      };
+    }
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -52,10 +51,13 @@ $('#new_message').on('submit', function(e){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);      
-      $('form')[0].reset();
-    })
+     .done(function(data){
+       var html = buildHTML(data);
+       console.log(html);
+       $('.chat-main__message-list').append(html);
+       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight}); 
+       $('.submit-btn').prop("disabled", false);     
+       $('form')[0].reset();
+     })
 })
 });
