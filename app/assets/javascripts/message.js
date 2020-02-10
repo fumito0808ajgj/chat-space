48b078d7d6fc17de61ee6f31788203a406103dad
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-02-10 11:48:13 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+     function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-info">
+            <div class="name">
+              ${message.user_name}
+            </div>
+            <date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="post">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+       `<div class="message" data-message-id=${message.id}>
+          <div class="message-info">
+            <div class="name">
+              ${message.user_name}
+            </div>
+            <div class="date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="post">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
