3e1e603b7c663c96b80cacd8fb36c1c6a585b4bf
fumito0808ajgj <42643727+fumito0808ajgj@users.noreply.github.com>
2020-02-17 15:28:48 +0900

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
       format.json
     end
   end
+  end
 
   def update
     if current_user.update(user_params)
@@ -22,6 +23,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 
 end
